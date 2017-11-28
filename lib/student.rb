d2632c8a918ef23dbbc4573d7e5ba517b76181be
Amanda Ackerman <ackerm44@gmail.com>
2017-11-28 15:55:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,9 @@ class Student
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  
 end
